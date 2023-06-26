33459df7a6faa60c0a6ecbe0a0cf212a1a2e7678
JungHoon <meslee70@gmail.com>
2023-06-26 10:08:16 +0900

    jwt 생성

--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -1,18 +1,20 @@
 package com.sparta.blog.controller;
 
 import com.sparta.blog.dto.UserRequestDto;
+import com.sparta.blog.jwt.JwtUtil;
 import com.sparta.blog.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping
+@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
-    public UserController(UserService userService){
+    private final JwtUtil jwtUtil;
+    public UserController(UserService userService,JwtUtil jwtUtil){
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
 
     @PostMapping("/auth/signup")
@@ -23,5 +25,35 @@ public class UserController {
     @PostMapping("/auth/login")
     public String loginUser(@RequestBody UserRequestDto userRequestDto){
         return userService.loginUser(userRequestDto);
+    }
+    @GetMapping("/create-jwt")
+    public String createJwt(HttpServletResponse res) {
+        // Jwt 생성
+        String token = jwtUtil.createToken("JungHoon");
+
+        // Jwt 쿠키 저장
+        jwtUtil.addJwtToCookie(token, res);
+
+        return "createJwt : " + token;
+    }
+
+    @GetMapping("/get-jwt")
+    public String getJwt(@CookieValue(JwtUtil.AUTHORIZATION_HEADER) String tokenValue) {
+        // JWT 토큰 substring
+        String token = jwtUtil.substringToken(tokenValue);
+
+        // 토큰 검증
+        if(!jwtUtil.validateToken(token)){
+            throw new IllegalArgumentException("Token Error");
+        }
+
+        // 토큰에서 사용자 정보 가져오기
+        Claims info = jwtUtil.getUserInfoFromToken(token);
+        // 사용자 username
+        String username = info.getSubject();
+        System.out.println("username = " + username);
+
+
+        return "getJwt : " + username ;
     }
 }
